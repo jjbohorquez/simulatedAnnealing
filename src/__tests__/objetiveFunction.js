75e3d51b6b27bdabea7f7b51b21651a38a79eee9
jobo322 <jose.bolanos@correounivalle.edu.co>
2018-09-01 12:02:50 -0500

    test case form and configuration

--- src/__tests__/objetiveFunction.js
+++ src/__tests__/objetiveFunction.js
@@ -1,21 +1,22 @@
+import { toBeDeepCloseTo } from 'jest-matcher-deep-close-to';
 import simulatedAnnealing from '..';
+expect.extend({ toBeDeepCloseTo });
 
-function polinom(x){
-    let y = Math.pow(x[0],4) - 16 * Math.pow(x[0],2) - x[0] * 5 +  Math.pow(x[1],4) - 16 * Math.pow(x[1],2) - x[1] * 5;
-    return y
-}
-
-let simulatedAnnealingInputs = {
-    goalFunction : polinom,
-    guess : [0, 0],
-    neighbour : {
-    // lowerBound : [-5, -5] ,
-    upperBound : [5, 5],
-    },
-    maxIterations : 10000,
-    quenchingFactor :2
+test('one variable polinom', () => {
+    function polinom(x){
+        let y = Math.pow(x[0],4) - 16 * Math.pow(x[0],2) - x[0] * 5 +  Math.pow(x[1],4) - 16 * Math.pow(x[1],2) - x[1] * 5;
+        return y
+    }
     
-}
-let simulatedAnnealingOutputs = simulatedAnnealing(simulatedAnnealingInputs);
-
-let idealValues = [[2.9029, 2.9028], -156.66];
+    let simulatedAnnealingInputs = {
+        goalFunction : polinom,
+        guess : [0, 0],
+        neighbour : {
+        // lowerBound : [-5, -5] ,
+        upperBound : [5, 5],
+        },
+        maxIterations : 10000,
+        quenchingFactor :2   
+    }
+    expect(simulatedAnnealing(simulatedAnnealingInputs)).toBeDeepCloseTo([[2.9029, 2.9028], -156.66], 2)
+});
